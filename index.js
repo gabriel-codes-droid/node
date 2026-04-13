3d10a2d9e213ee1f26086cca3ec0f68378672a68
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-13 22:30:04 +0200

    .

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const {readFile,readFileSync} = require('fs');
 const txt = readFileSync('./hello.txt','utf-8');
 console.log(txt);
+const generateNumber =require('./utils');
+console.log(`Your random number is : ${generateNumber()}`);
