6b0f0064710acd755fb6c99b7a4b9d9390f93cc9
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-20 08:58:07 +0200

    .

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import http from 'http'
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req,res)=>{
 // res.setHeader('Content-Type','text/html')
 // res.statusCode =404 ;
+console.log(req.url);
+console.log(req.method);
 
-res.writeHead(400, {'Content-Type':'application/json'})
+
+res.writeHead(200, {'Content-Type':'text/html'})
 //res.end("<h1>Hello ma guy</h1>")
-res.end(JSON.stringify({message :'Server Error '}))
+res.end('Hi hello how re u doing')
 });
 server.listen(PORT,()=>{
 console.log(`Server running at port ${PORT}`);
