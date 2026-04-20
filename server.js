8924a98ecc9c3538958f7efcdca74513cc78216d
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-20 21:31:01 +0200

    .

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const server = http.createServer((req,res)=>{
 // res.statusCode =404 ;
 // console.log(req.url);
 // console.log(req.method);
-if (req.url==='/'){
+try{
+    if(req.method ==='GET'){
+      if (req.url==='/'){
 
 res.writeHead(200, {'Content-Type':'text/html'})
 res.end("<h1>Home page</h1>")
@@ -18,7 +20,17 @@ else if (req.url==='/about'){
 else {
     res.writeHead(404, {'Content-Type':'text/html'})
     res.end("<h1>Not found </h1>")
+}  
+    }
+    else{
+        throw new Error ('Method not allowed')
+    }
+} catch (error){
+    res.writeHead(500,{'Content-Type':'text/plain'})
+    res.end('Server Error')
 }
+
+
 
 });
 server.listen(PORT,()=>{
