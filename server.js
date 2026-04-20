f3caa5d3ba78fc87ad96a0200109fb3810d5437c
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-20 17:05:08 +0200

    .

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ const PORT = process.env.PORT;
 const server = http.createServer((req,res)=>{
 // res.setHeader('Content-Type','text/html')
 // res.statusCode =404 ;
-console.log(req.url);
-console.log(req.method);
-
+// console.log(req.url);
+// console.log(req.method);
+if (req.url==='/'){
 
 res.writeHead(200, {'Content-Type':'text/html'})
-//res.end("<h1>Hello ma guy</h1>")
-res.end('Hi hello how re u doing')
+res.end("<h1>Home page</h1>")
+}
+else if (req.url==='/about'){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.end("<h1>About</h1>")
+}
+else {
+    res.writeHead(404, {'Content-Type':'text/html'})
+    res.end("<h1>Not found </h1>")
+}
+
 });
 server.listen(PORT,()=>{
 console.log(`Server running at port ${PORT}`);
