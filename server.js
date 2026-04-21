f5e6a60c387283041d6720f4c53ebf53426c57b5
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-21 17:13:20 +0200

    .

--- server.js
+++ server.js
@@ -7,26 +7,27 @@ const PORT = process.env.PORT;
 const __filename =url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__filename,__dirname)
-const server = http.createServer((req,res)=>{
+const server = http.createServer(async(req,res)=>{
 // res.setHeader('Content-Type','text/html')
 // res.statusCode =404 ;
 // console.log(req.url);
 // console.log(req.method);
 try{
     if(req.method ==='GET'){
+        let filePath;
       if (req.url==='/'){
-
-res.writeHead(200, {'Content-Type':'text/html'})
-res.end("<h1>Home page</h1>")
+        filePath = path.join(__dirname,"public",'index.html')
 }
 else if (req.url==='/about'){
-    res.writeHead(200, {'Content-Type':'text/html'})
-    res.end("<h1>About</h1>")
+   filePath = path.join(__dirname,"public",'about.html')
 }
 else {
-    res.writeHead(404, {'Content-Type':'text/html'})
-    res.end("<h1>Not found </h1>")
-}  
+  throw new Error ('Not found')
+}
+const data = await fs.readFile(filePath);
+res.setHeader('Content-Type','text/html');
+res.write(data);
+res.end();
     }
     else{
         throw new Error ('Method not allowed')
