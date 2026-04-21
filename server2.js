428ae1b3b0dfdc027fc1adb6a9a6f98cb998fc40
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-21 20:48:43 +0200

    .

--- server2.js
+++ server2.js
@@ -1,12 +1,17 @@
 import { createServer} from 'http'
-const PORT =process.env.PORT
+const PORT =process.env.PORT;
+
 const users =[
     {id: 1, name :'John Doe'},
     {id: 2, name :'Jim Doe'},
     {id: 3, name :'Jane Doe'},
 ];
 const server =createServer((req,res)=>{
-
+if (req.url==='/api/users'&& req.method === 'GET'){
+    res.setHeader('Content-Type','application/json');
+    res.write(JSON.stringify(users));
+    res.end();
+}
 });
 server.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
