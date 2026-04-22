53bd4275d202faee2f2892c5395255b99d04e74b
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-22 09:28:39 +0200

    .

--- server2.js
+++ server2.js
@@ -12,6 +12,21 @@ if (req.url==='/api/users'&& req.method === 'GET'){
     res.write(JSON.stringify(users));
     res.end();
 }
+else if (req.url.match(/\/api\/users\/([0-9]+)/) &&req.method==='GET'){
+const id =req.url.split('/')[3];
+const user =users.find((user)=>user.id===parseInt(id));
+
+if (user){
+    res.setHeader('Content-Type','application/json');
+    res.write(JSON.stringify({id:1, name : 'John Doe'}));
+    res.end();
+} else{
+  res.setHeader('Content-Type','application/json');
+    res.write(JSON.stringify({message: 'Route not found'}));
+    res.end(); 
+}
+
+}
 else{
     res.setHeader('Content-Type','application/json');
     res.write(JSON.stringify({message: 'Route not found'}));
@@ -20,4 +35,4 @@ else{
 });
 server.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
-})
+}) 
