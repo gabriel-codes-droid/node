d52b1b5d6d15dbdbabe59ec34fbc0ab8f760b5e1
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-23 21:43:13 +0200

    .

--- server2.js
+++ server2.js
@@ -10,9 +10,39 @@ const logger =(req,res,next)=>{
     console.log(`${req.method} ${req.url}`);
     next();
 }
+//JSON Middleware
+const jsonMiddleware = (req,res ,next)=>{
+    res.setHeader('Content-Type','application/json');
+    next();
+}
+
+//Route handler for Get /api/users
+const getUsetrHandler = (req,res)=>{
+    res.write(JSON.stringify(users));
+    res.end();
+}
+// Route handler fro get /api/users/:id 
+const getUsersByHandler =(req,res)=>{
+   const id =req.url.split('/')[3];
+   const user =users.find((user)=>user.id===parseInt(id)); 
+   if (user){
+    res.write(JSON.stringify(user));
+} else{
+    res.statusCode = 404 ;
+    res.write(JSON.stringify({message: 'User not found'}));
+}
+res.end();
+}
+//Not found Handler
+const notFoundHandler =(req,res)=>{
+    res.write(JSON.stringify({message: 'Route not found'}));
+    res.end(); 
+}
+
 const server =createServer((req,res)=>{
     logger (req,res ,()=>{
-        if (req.url==='/api/users'&& req.method === 'GET'){
+        if (req.url==='/api/users'&&
+             req.method === 'GET'){
     res.setHeader('Content-Type','application/json');
     res.write(JSON.stringify(users));
     res.end();
