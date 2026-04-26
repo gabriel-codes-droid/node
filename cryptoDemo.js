057c8b171191099802cb0ed34c21ab289d8dd0ea
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-26 21:51:58 +0200

    .

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -20,3 +20,8 @@ const cipher = crypto.createCipheriv(algorithm, key, iv);
 let encrypted = cipher.update('Hello, this is a secret message', 'utf8', 'hex');
 encrypted += cipher.final('hex');
 console.log(encrypted);
+
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+decrypted += decipher.final('utf8');
+console.log(decrypted);
