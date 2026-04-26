5192222c21424a0e49c4d21dbb8f24a331dbb1ff
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-26 21:45:48 +0200

    .

---
+++ cryptoDemo.js
@@ -0,0 +1,22 @@
+import crypto from 'crypto'
+
+//createHash()
+// const hash =crypto.createHash('sha256')
+// hash.update('password1234');
+// console.log(hash.digest('hex'));
+
+// //randmolyBytes()
+// crypto.randomBytes(16 , (err , buf)=>{
+//     if (err)throw err;
+//     console.log(buf.toString('hex'));
+// });
+
+//createCipheriv & createDecipheriv
+const algorithm = 'aes-256-cbc';
+const key =crypto.randomBytes(32);
+const iv  = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encrypted = cipher.update('Hello, this is a secret message', 'utf8', 'hex');
+encrypted += cipher.final('hex');
+console.log(encrypted);
