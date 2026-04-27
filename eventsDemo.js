b80b1742643e06234f8f4ecd4c209c003786b15d
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-04-27 07:31:04 +0200

    .

--- eventsDemo.js
+++ eventsDemo.js
@@ -2,16 +2,16 @@ import {EventEmitter} from 'events';
 
 const myEmitter = new EventEmitter();
 
-function greetHandler(){
-    console.log('hello World')
+function greetHandler(name){
+    console.log('hello '+ name)
 }
-function goodbyeHandler(){
-    console.log('goodbye World')
+function goodbyeHandler(name){
+    console.log('goodbye '+ name)
 }
 
 myEmitter.on('greet',greetHandler);
 myEmitter.on('goodbye',goodbyeHandler);
 
-//Emit events
-myEmitter.emit('greet');
-myEmitter.emit('goodbye');
+//Emit events,
+myEmitter.emit('greet' , 'John');
+myEmitter.emit('goodbye' , 'John');
